6d776aaef4e3a22abc7cbd33ee9570d8e9554925
salihva <salihva777@gmail.com>
2024-02-20 13:25:02 +0530

    first commit

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import Greeting from "./components/Greeting"
+import Greeting1 from "./components/Greeting1"
+
+
+function App() {
+  return (
+    <>
+    <h1>hello</h1>
+   <Greeting name={"shank"}/>
+   <Greeting1 name={"john"}/>
+
+    </>
+  )
+}
+
+export default App
