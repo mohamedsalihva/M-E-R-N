f4922d6875cefcffd9d85c38406d3974212d1acd
salihva <salihva777@gmail.com>
2024-02-24 13:10:09 +0530

    first commit

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import Navbar from './components/navbar/Navbar';
-import Loginpage from './components/loginpage/Loginpage';
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+// import Navbar from './components/navbar/Navbar';
+// import Loginpage from './components/loginpage/Loginpage';
+// import Purecomponent1 from "./components/purecomponents/Purecomponent1";
+import Purecomponent2 from "./components/purecomponents/Purecomponent2";
+
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes> 
-          <Route path="/Loginpage" element={<Loginpage />} />
-
-        </Routes>
-      </div>
-    </Router>
+  //  <Purecomponent1 message="hello"/>
+  <Purecomponent2 message="hello2"/>
   );
 };
 
