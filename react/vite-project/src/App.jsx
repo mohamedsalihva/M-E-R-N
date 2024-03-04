fe971343a7f9800dfbb6f18b4a6108c3a1dc9da5
mosalihva <salihva777@gmail.com>
2024-03-04 13:49:44 +0530

    HOOKS

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -1,26 +1,8 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/landingpage/Landingpage';
-import LoginPage from './components/landingpage/Loginpage';
-import AddUserPage from './components/landingpage/Adduserpage';
-// import Navbar from './components/landingpage/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/landingpage/Footer';
-
-
+import Usecallbackcomponent from "./components/Hooks/Usecallback/Usecallbackcomponent";
 
 const App = () => {
   return (
-    <Router>
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/add-user" element={<AddUserPage />} />
-      </Routes>
-      {/* <Footer/> */}
-    </Router>
+  <Usecallbackcomponent/>
   );
 };
 
